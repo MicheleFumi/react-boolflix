3c1673feac6ce6aabcf35f62aad52775e26c89d6
MicheleFumi <michele.fumi96@gmail.com>
2024-11-28 17:16:24 +0100

    fix milestone 2 flag part

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
-import React, { useEffect } from 'react';
-import { useMovieContext } from './Context/MyContextData';  // Importa il contesto
-import './app.css'
+import React from 'react';
+import { useMovieContext } from './Context/MyContextData'; // Importa il contesto
+import './app.css';
 import Flag from 'react-country-flag';
 
-
 export default function App() {
-  const { movies, searchQuery, setSearchQuery, fetchMovies, error } = useMovieContext();
-  const handleInputChange = (event) => {
-    setSearchQuery(event.target.value);  // Aggiorna la query
-    console.log(event.target.value);
+  const { movies, searchQuery, setSearchQuery, fetchMovies } = useMovieContext();
 
+  const handleInputChange = (event) => {
+    setSearchQuery(event.target.value);
   };
 
   const handleSearchSubmit = (event) => {
-    event.preventDefault();  // Evita il comportamento di default del form
-    fetchMovies(searchQuery);  // Fa la ricerca usando la query
-    console.log(fetchMovies);
-
+    event.preventDefault();
+    fetchMovies(searchQuery);
   };
-
 
   return (
     <div>
@@ -35,29 +30,38 @@ export default function App() {
         <button type="submit">Cerca</button>
       </form>
 
-
-
       {/* Mostriamo i film */}
       <div>
         {movies.length > 0 ? (
           <ul>
-            {movies.map((movie) => (
-              <li key={movie.id} >
-                <h3>Titolo: {movie.title}</h3>
-                <p>Titolo Originale: {movie.original_title}</p>
-                <Flag countryCode={movie.original_language} style={{ width: 30, height: 30 }}></Flag>
-                <p>Voto: {movie.vote_average}</p>
-              </li>
+            {movies.map((movie) => {
+              let countryCode = movie.original_language.toUpperCase();
 
+              // Gestione delle eccezioni per lingue specifiche
+              if (countryCode === 'EN') {
+                countryCode = 'US'; // Inglese -> Regno Unito
+              } else if (countryCode === 'ZH') {
+                countryCode = 'CN'; // Cinese -> Cina
+              } else if (countryCode === 'JA') {
+                countryCode = 'JP'; // Giapponese -> Giappone
+              } else if (countryCode === 'KO') {
+                countryCode = 'KR';
+              }
 
-            ))}
+              return (
+                <li key={movie.id}>
+                  <h3>Titolo: {movie.title}</h3>
+                  <p>Titolo Originale: {movie.original_title}</p>
+                  <Flag countryCode={countryCode} style={{ width: 50, height: 50 }} />
+                  <p>Voto: {movie.vote_average}</p>
+                </li>
+              );
+            })}
           </ul>
         ) : (
           <p>Nessun film trovato.</p>
         )}
       </div>
-    </div >
+    </div>
   );
-};
-
-
+}
